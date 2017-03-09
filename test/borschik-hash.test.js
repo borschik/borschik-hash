a0dd5950993b79781775380582f60bae389ad6f3
blond <andrewblond@yandex.com>
2017-03-09 15:26:58 +0300

    test: fix borschikHash tests

--- test/borschik-hash.test.js
+++ test/borschik-hash.test.js
@@ -7,29 +7,31 @@ var borschikHash = require('../index');
 
 var FIXTURES_DIR = path.join(__dirname, 'fixtures');
 
-it('should hash contents of CSS file', function() {
-    var filename = path.join(FIXTURES_DIR, 'text.css');
-    var contents = fs.readFileSync(filename);
+describe('borschikHash', function() {
+    it('should hash contents of CSS file', function() {
+        var filename = path.join(FIXTURES_DIR, 'text.css');
+        var contents = fs.readFileSync(filename);
 
-    var hash = borschikHash(contents);
+        var hash = borschikHash(contents);
 
-    assert(hash, 'LWJmVQ8Q4Kn5C4mK3iYXHyieR7g');
-});
+        assert.equal(hash, 'LWJmVQ8Q4Kn5C4mK3iYXHyieR7g');
+    });
 
-it('should hash contents of JS file', function() {
-    var filename = path.join(FIXTURES_DIR, 'text.js');
-    var contents = fs.readFileSync(filename);
+    it('should hash contents of JS file', function() {
+        var filename = path.join(FIXTURES_DIR, 'text.js');
+        var contents = fs.readFileSync(filename);
 
-    var hash = borschikHash(contents);
+        var hash = borschikHash(contents);
 
-    assert(hash, 's2waZ-cd23dy_WqyHXmzbhscY_k');
-});
+        assert.equal(hash, 's2waZ-cd23dy_WqyHXmzbhscY_k');
+    });
 
-it('should hash contents of png file', function() {
-    var filename = path.join(FIXTURES_DIR, 'image.png');
-    var contents = fs.readFileSync(filename);
+    it('should hash contents of png file', function() {
+        var filename = path.join(FIXTURES_DIR, 'image.png');
+        var contents = fs.readFileSync(filename);
 
-    var hash = borschikHash(contents);
+        var hash = borschikHash(contents);
 
-    assert(hash, 'wFPs-e1B3wMRud8TzGw7YHjS08I');
+        assert.equal(hash, 'wFPs-e1B3wMRud8TzGw7YHjS08I');
+    });
 });
